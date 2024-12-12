8c64a0ffa3eb65ae025fd48dd8469e0c694581cd
JBurghoffCE <j.burghoff@controlexpert.com>
2024-12-12 12:10:41 +0100

    Update postprocessing_invalidity_component.py

--- src/dc_custom_component/components/postprocessing_invalidity_component.py
+++ src/dc_custom_component/components/postprocessing_invalidity_component.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 
 from haystack import component
 
@@ -6,7 +6,7 @@ from haystack import component
 @component
 class PostprocessingInvalidityComponent:
     @component.output_types(valid_query=str, invalid_query=str)
-    def run(self, query: str, replies: List[str]):
+    def run(self, query: str, replies: List[str]) -> Dict[str, str]:
         if replies[0] == "valid":
             return {"valid_query": query}
         else:
